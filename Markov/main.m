e64170575e8b932f596e72a9e8e12566607d5fd3
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-14 19:09:20 -0600

    Initial Commit

---
+++ Markov/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Markov
+//
+//  Created by Phillip Tennen on 6/14/16.
+//  Copyright © 2016 Phillip Tennen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
