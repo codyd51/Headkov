a9e66d3e2c0b3b7e4cc1f2e57feced2e2f654237
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-16 20:11:43 -0700

    Refactor string generator

---
+++ Markov/MKVStringGenerator.h
@@ -0,0 +1,17 @@
+//
+//  MKVStringGenerator.h
+//  Markov
+//
+//  Created by Phillip Tennen on 6/16/16.
+//  Copyright © 2016 Phillip Tennen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKVStringGenerator : NSObject {
+    NSMutableDictionary* _states;
+}
+@property (nonatomic, retain) NSArray* tokens;
+-(instancetype)initWithBaseString:(NSString*)string;
+-(NSString*)generateString:(int)wordCount;
+@end
---
+++ Markov/MKVStringGenerator.m
@@ -0,0 +1,60 @@
+//
+//  MKVStringGenerator.m
+//  Markov
+//
+//  Created by Phillip Tennen on 6/16/16.
+//  Copyright © 2016 Phillip Tennen. All rights reserved.
+//
+
+#import "MKVStringGenerator.h"
+
+@implementation MKVStringGenerator
+-(instancetype)initWithBaseString:(NSString*)input {
+    if ((self = [super init])) {
+        //tokenize input string
+        self.tokens = [input componentsSeparatedByString:@" "];
+        
+        //for every token in the input string, add an entry if one doesn't exist and add the following token to the possibilities for that state
+        _states = [[NSMutableDictionary alloc] initWithCapacity:self.tokens.count];
+        for (int i = 0; i < self.tokens.count; i++) {
+            NSString* str = [self.tokens objectAtIndex:i];
+            
+            //key didn't exist yet, add it
+            if (!_states[str]) {
+                [_states setObject:[NSMutableArray new] forKey:str];
+            }
+            
+            //add next token to possible states for this key
+            NSMutableArray<NSString*>* possibilities = _states[str];
+            //the last key does not have a next state so add empty possibility for that case
+            if (i != self.tokens.count - 1) {
+                NSString* next = [self.tokens objectAtIndex:i+1];
+                [possibilities addObject:next];
+            } else [possibilities addObject:@""];
+        }
+    }
+    return self;
+}
+-(NSString*)generateString:(int)wordCount {
+    NSUInteger previousIdx = 0;
+    NSMutableString* result = [NSMutableString new];
+    for (NSUInteger i = 0; i < wordCount; i++) {
+        //start with token at the previous index
+        NSString* str = self.tokens[previousIdx];
+        [result appendString:[NSString stringWithFormat:@"%@ ", str]];
+        
+        //now, find the next token to use
+        //to do so, randomly chose one of the possible tokens from this state
+        NSMutableArray* possibilities = _states[str];
+        NSString* next = ([possibilities count] == 0) ? @"" : [possibilities objectAtIndex:arc4random_uniform([possibilities count])];
+        //NSString* next = possibilities[arc4random_uniform(possibilities.count)];
+        
+        //set the next index to this one
+        if ([self.tokens indexOfObject:next] != NSNotFound) {
+            previousIdx = [self.tokens indexOfObject:next];
+        } else previousIdx = 0;
+    }
+    
+    return [NSString stringWithString:result];
+}
+@end
